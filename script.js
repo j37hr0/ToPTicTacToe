58cb417e848871b89b9b1023f79569e89d73d403
j37hr0 <jethro.cotton3@gmail.com>
2022-10-14 13:39:35 +0200

    got some stuff working

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 const gameboard = document.getElementById("gameboard")
 
 
+//globals
+var gameState = [0,0,0,0,0,0,0,0,0];
+const winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+];
+
+
 //player factory function
 const createPlayer = (name, marker) => {
     return {name, marker}
@@ -9,52 +23,58 @@ const createPlayer = (name, marker) => {
 
 //gameboard module
 const gameBoard = (function() {
-    // TODO add event listeners
-    for (let i = 0; i < 9; i++){
-    let square = document.getElementById(`${i}`);
-    square.addEventListener('click', () => {
-        game.gameState[square.id] = activePlayer.marker;
-        gameBoard.display(gameState)
-    }
-}
     return {
         display: function(state) {
             for (let i = 0; i < 9; i++){
                 let block = document.getElementById(state[i]);
-                if (state[i] === 0){
-                }
-                else if (state[i] === 1){
+                if (state[i] === 1){
                     block.innerHTML = "X"
                 }
                 else if (state[i] === 2){
                     block.innerHTML = "O";
                 }
+                else if (state[i] === 0){
+                }
         }
-    } 
+    },
+    // play: function() {
+    //     newID = this.id;
+    //     gameState[newID] = activePlayer.marker;
+    //     gameBoard.display();
+    // }
 };
 })();
 
 //game object
 const game = () => {
-    let gameState = [0,0,0,0,0,0,0,0,0];
-    const winConditions = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [6,4,2]
-    ];
     const playerOne = createPlayer("player one", 1);
     const playerTwo = createPlayer("player Two", 2);
-
     let activePlayer = playerOne;
     let hasWon = false;
 
-
+    return {activePlayer, hasWon}
 }
 
 
-displayBoard.display(gameState)
+
+
+
+gameBoard.display(gameState)
+
+
+// add event listeners
+for (let i = 0; i < 9; i++){
+    let sqauare = document.getElementById(`${i}`);
+    sqauare.onclick = function () {
+        if (game.activePlayer === game.playerOne){
+            gameState[i] = 1;
+            gameBoard.display(gameState);
+        }
+        else if (game.activePlayer === game.playerTwo) {
+            gameState[i] = 2;
+            gameBoard.display(gameState);
+        }
+    }
+}
+
+
